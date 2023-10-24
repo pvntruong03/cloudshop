715162a55e1f31da3dd1188b003c56c9836cd899
pvntruong03 <pvntruongit03@gmail.com>
2023-10-24 15:53:45 +0700

    upate

--- detail.php
+++ detail.php
@@ -22,11 +22,6 @@ if (isset($_GET['delete_id'])) {
         echo "Failed to delete the product.";
     }
 }
-if(isset($_SESSION["user_name"])) {
-    $users = $_SESSION['user_name'];
-}else{
-    header('Location: login.php');
-}
 ?>
 
 <div class="container mx-auto">
