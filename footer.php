47fb19f62f206cbfa531888db364990b9bff33ff
pvntruong03 <pvntruongit03@gmail.com>
2023-10-26 07:58:46 +0700

    up

--- footer.php
+++ footer.php
@@ -2,14 +2,12 @@
     <!--Footer-->
     <footer class="text-muted py-5">
         <div class="container">
-            <p class="float-end mb-1">
-                <a href="#" class="text-decoration-none">Back to top</a>
-            </p>
-            <p>
-                &copy; 2017-2022 Company, Inc. &middot; 
-                <a href="#" class="text-decoration-none">Privacy</a> &middot; 
-                <a href="#" class="text-decoration-none">Terms</a>>
-            </p>
+            <h2>Adress</h2>
+                    <p>842 Su Van Hanh, Ward 12, District 10, HCMC</p>
+                    <p>54, Mau Than Street,Hung Vuong, Can Tho</p>
+                    <p>9,Nguyen Trai Street,Ca Mau</p>
+                    <p>10,Binh Dai District, Ben Tre</p>
+                    <p>&copy; 2017-2022 company, Inc.Terms Privacy&middot;</p><p class="float-end mb-1"><a href="#">Back to top</a></p>
         </div>
     </footer>
 </html>
